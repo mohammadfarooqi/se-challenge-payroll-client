f4a4e6aa0933dad375a955af1a892a92dd2ff3ec
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-23 17:29:16 -0500

    added nav and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './client/home/home.component';
+import { UploadComponent } from './client/upload/upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UploadComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: HomeComponent
+      },
+      {
+        path: 'upload', component: UploadComponent
+      }
+    ]),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
